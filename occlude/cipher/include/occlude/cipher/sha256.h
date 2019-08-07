91d9d0a2f458bfac4ef05e7c88f8671329910b34
Peter Bindels <peter.bindels@tomtom.com>
2019-08-07 14:10:02 +0200

    Add sha256 tests, fix implementation to both work and be value-typed (incl test)

--- occlude/cipher/include/occlude/cipher/sha256.h
+++ occlude/cipher/include/occlude/cipher/sha256.h
@@ -1,16 +1,26 @@
 #pragma once
 
-#include <vector>
-#include <span>
+#include <array>
 #include <cstddef>
+#include <vector>
 
 namespace Occlude::Cipher {
 
 struct Sha256 {
-  operator std::vector<uint8_t>();
+  void addBytes(const uint8_t* data, size_t length);
+  operator std::vector<uint8_t>() const;
   size_t byteCount = 0;
   std::array<uint32_t, 8> state = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
+  std::array<uint32_t, 16> data = {};
+private:
+  void transform();
 };
+
+inline std::vector<uint8_t> sha256(const uint8_t* data, size_t length) {
+  Sha256 obj;
+  obj.addBytes(data, length);
+  return obj;
+}
 
 }
 
--- occlude/cipher/src/sha256.cpp
+++ occlude/cipher/src/sha256.cpp
@@ -1,64 +1,43 @@
 /* Based on code from Igor Pavlov in the Public domain */
-#include "Sha256.h"
+#include "occlude/cipher/sha256.h"
 
-static void Sha256_Transform(uint32_t *state, const uint32_t *data);
-
-static void Sha256_WriteByteBlock(CSha256 *p)
-{
-  uint32_t data32[16];
-  unsigned i;
-  for (i = 0; i < 16; i++)
-    data32[i] =
-      ((uint32_t)(p->buffer[i * 4    ]) << 24) +
-      ((uint32_t)(p->buffer[i * 4 + 1]) << 16) +
-      ((uint32_t)(p->buffer[i * 4 + 2]) <<  8) +
-      ((uint32_t)(p->buffer[i * 4 + 3]));
-  Sha256_Transform(p->state, data32);
-}
-
-void Sha256_Update(CSha256 *p, const uint8_t *data, size_t size)
-{
-  uint32_t curBufferPos = (uint32_t)p->count & 0x3F;
-  while (size > 0)
-  {
-    p->buffer[curBufferPos++] = *data++;
-    p->count++;
-    size--;
-    if (curBufferPos == 64)
-    {
-      curBufferPos = 0;
-      Sha256_WriteByteBlock(p);
+void Occlude::Cipher::Sha256::addBytes(const uint8_t* newdata, size_t length) {
+  for (size_t i = 0; i < length; i++) {
+    size_t byteindex = (byteCount) % 64;
+    data[byteindex / 4] |= ((uint32_t)newdata[i] << (8*(3-byteindex % 4)));
+    if ((++byteCount % 64) == 0) {
+      transform();
     }
   }
 }
 
-void Sha256_Final(CSha256 *p, uint8_t *digest)
-{
-  uint64_t lenInBits = (p->count << 3);
-  uint32_t curBufferPos = (uint32_t)p->count & 0x3F;
-  unsigned i;
-  p->buffer[curBufferPos++] = 0x80;
-  while (curBufferPos != (64 - 8))
-  {
-    curBufferPos &= 0x3F;
-    if (curBufferPos == 0)
-      Sha256_WriteByteBlock(p);
-    p->buffer[curBufferPos++] = 0;
-  }
-  for (i = 0; i < 8; i++)
-  {
-    p->buffer[curBufferPos++] = (uint8_t)(lenInBits >> 56);
-    lenInBits <<= 8;
-  }
-  Sha256_WriteByteBlock(p);
+Occlude::Cipher::Sha256::operator std::vector<uint8_t>() const {
+  Sha256 copy = *this;
 
-  for (i = 0; i < 8; i++)
+  uint8_t blank[64] = {0x80};
+  copy.addBytes(blank, (byteCount % 64) > 55 ? 120 - (byteCount % 64): 56 - (byteCount % 64));
+ 
+  uint8_t lenInBits[8] = { 
+    (uint8_t)((byteCount >> 53) & 0xFF), 
+    (uint8_t)((byteCount >> 45) & 0xFF),
+    (uint8_t)((byteCount >> 37) & 0xFF),
+    (uint8_t)((byteCount >> 29) & 0xFF),
+    (uint8_t)((byteCount >> 21) & 0xFF),
+    (uint8_t)((byteCount >> 13) & 0xFF),
+    (uint8_t)((byteCount >> 5) & 0xFF),
+    (uint8_t)((byteCount << 3) & 0xFF)
+  };
+  copy.addBytes(lenInBits, 8);
+
+  std::vector<uint8_t> hash;
+  for (size_t i = 0; i < 8; i++)
   {
-    *digest++ = (uint8_t)(p->state[i] >> 24);
-    *digest++ = (uint8_t)(p->state[i] >> 16);
-    *digest++ = (uint8_t)(p->state[i] >> 8);
-    *digest++ = (uint8_t)(p->state[i]);
+    hash.push_back((uint8_t)(copy.state[i] >> 24));
+    hash.push_back((uint8_t)(copy.state[i] >> 16));
+    hash.push_back((uint8_t)(copy.state[i] >> 8));
+    hash.push_back((uint8_t)(copy.state[i]));
   }
+  return hash;
 }
 
 static const uint32_t K[64] = {
@@ -80,18 +59,33 @@ static const uint32_t K[64] = {
   0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
-#define rotr(x, c) ((x >> c) | (x << 32-c))
+static inline uint32_t rotr(uint32_t value, uint8_t rotation) {
+  return (value >> rotation) | (value << (32 - rotation));
+}
 
-#define S0(x) (rotr(x, 2) ^ rotr(x,13) ^ rotr(x, 22))
-#define S1(x) (rotr(x, 6) ^ rotr(x,11) ^ rotr(x, 25))
-#define s0(x) (rotr(x, 7) ^ rotr(x,18) ^ (x >> 3))
-#define s1(x) (rotr(x,17) ^ rotr(x,19) ^ (x >> 10))
+inline uint32_t S0(uint32_t x) {
+  return rotr(x, 2) ^ rotr(x,13) ^ rotr(x, 22);
+}
 
-#define blk0(i) (W[i] = data[i])
-#define blk2(i) (W[i&15] += s1(W[(i-2)&15]) + W[(i-7)&15] + s0(W[(i-15)&15]))
+inline uint32_t S1(uint32_t x) {
+  return rotr(x, 6) ^ rotr(x,11) ^ rotr(x, 25);
+}
 
-#define Ch(x,y,z) (z^(x&(y^z)))
-#define Maj(x,y,z) ((x&y)|(z&(x|y)))
+inline uint32_t s0(uint32_t x) {
+  return rotr(x, 7) ^ rotr(x,18) ^ (x >> 3);
+}
+
+inline uint32_t s1(uint32_t x) {
+  return rotr(x,17) ^ rotr(x,19) ^ (x >> 10);
+}
+
+inline uint32_t Ch(uint32_t x,uint32_t y,uint32_t z) {
+  return z^(x&(y^z));
+}
+
+inline uint32_t Maj(uint32_t x,uint32_t y,uint32_t z) {
+  return (x&y)|(z&(x|y));
+}
 
 #define a(i) T[(0-(i))&7]
 #define b(i) T[(1-(i))&7]
@@ -102,22 +96,31 @@ static const uint32_t K[64] = {
 #define g(i) T[(6-(i))&7]
 #define h(i) T[(7-(i))&7]
 
-static void Sha256_Transform(uint32_t *state, const uint32_t *data)
+void Occlude::Cipher::Sha256::transform()
 {
-  uint32_t W[16];
   uint32_t T[8];
   for (size_t j = 0; j < 8; j++)
     T[j] = state[j];
 
-  for (size_t j = 0; j < 64; j++)
+  for (size_t i = 0; i < 16; i++)
+  {
+    h(i) += S1(e(i)) + Ch(e(i),f(i),g(i)) + K[i] + data[i];
+    d(i) += h(i); 
+    h(i) += S0(a(i)) + Maj(a(i), b(i), c(i));
+  }
+  for (size_t j = 16; j < 64; j++)
   {
     size_t i = (j % 16);
-    h(i) += S1(e(i)) + Ch(e(i),f(i),g(i)) + K[j] + (j >= 15 ? blk2(i) : blk0(i));
+    data[i] += s1(data[(i+14)&15]) + data[(i+9)&15] + s0(data[(i+1)&15]);
+    h(i) += S1(e(i)) + Ch(e(i),f(i),g(i)) + K[j] + data[i];
     d(i) += h(i); 
     h(i) += S0(a(i)) + Maj(a(i), b(i), c(i));
   }
 
   for (size_t j = 0; j < 8; j++)
     state[j] += T[j];
+
+  data = {};
 }
+
 
