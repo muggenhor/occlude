9d3504eb9db189299ab5a1f80836592e12440399
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2019-08-10 08:27:00 +0200

    refactor: move AES key scheduling code out of header
    It shouldn't be used frequently so the inlining advantage isn't worth
    it.

--- occlude/cipher/include/occlude/cipher/aes.h
+++ occlude/cipher/include/occlude/cipher/aes.h
@@ -11,23 +11,7 @@ namespace Occlude::Cipher {
 
 class AesKeySchedule {
 public:
-  explicit AesKeySchedule(const std::vector<uint8_t>& key) {
-    const uint8_t roundConstants[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
-    switch(key.size()) {
-      case 16: keysize = Aes128; break;
-      case 24: // keysize = Aes192; break;
-      case 32: // keysize = Aes256; break;
-      default: abort();
-    }
-    memcpy(eroundKeys, key.data(), key.size());
-    for (size_t n = 0; n < 10; n++) {
-        __m128i t = eroundKeys[n] ^ _mm_srli_si128(_mm_aeskeygenassist_si128 (eroundKeys[n] ,roundConstants[n]), 0x0c);
-        eroundKeys[n+1] = t ^ _mm_slli_si128 (t, 0x4) ^ _mm_slli_si128 (t, 0x8) ^ _mm_slli_si128 (t, 0xC);
-    }
-    for (size_t n = 0; n < 9; n++) {
-        droundKeys[8 - n] = _mm_aesimc_si128(eroundKeys[n+1]);
-    } 
-  }
+  explicit AesKeySchedule(const std::vector<uint8_t>& key);
 //private:
   enum {
     Aes128,
@@ -59,4 +43,3 @@ __m128i AesDecrypt(const AesKeySchedule& key, __m128i block) {
 }
 
 }
-
---
+++ occlude/cipher/src/aes.cpp
@@ -0,0 +1,30 @@
+#include "occlude/cipher/aes.h"
+
+#include <x86intrin.h>
+#include <vector>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+namespace Occlude::Cipher {
+
+AesKeySchedule::AesKeySchedule(const std::vector<uint8_t>& key) {
+  const uint8_t roundConstants[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
+  switch(key.size()) {
+    case 16: keysize = Aes128; break;
+    case 24: // keysize = Aes192; break;
+    case 32: // keysize = Aes256; break;
+    default: abort();
+  }
+  memcpy(eroundKeys, key.data(), key.size());
+  for (size_t n = 0; n < 10; n++) {
+      __m128i t = eroundKeys[n] ^ _mm_srli_si128(_mm_aeskeygenassist_si128 (eroundKeys[n] ,roundConstants[n]), 0x0c);
+      eroundKeys[n+1] = t ^ _mm_slli_si128 (t, 0x4) ^ _mm_slli_si128 (t, 0x8) ^ _mm_slli_si128 (t, 0xC);
+  }
+  for (size_t n = 0; n < 9; n++) {
+      droundKeys[8 - n] = _mm_aesimc_si128(eroundKeys[n+1]);
+  }
+}
+
+}
