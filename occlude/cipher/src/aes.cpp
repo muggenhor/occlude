d4573b27bae7a8f1d79e3aae9df3036441889cff
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2019-08-10 09:04:47 +0200

    fix: make the AES round constants constexpr
    This array must be both 'static' and 'constexpr' for GCC 9.1 to stop
    complaining about the 'aeskeygenassist' intrinsic requiring
    an (8-bit) _immediate_.

--- occlude/cipher/src/aes.cpp
+++ occlude/cipher/src/aes.cpp
@@ -9,7 +9,7 @@
 namespace Occlude::Cipher {
 
 AesKeySchedule::AesKeySchedule(const std::vector<uint8_t>& key) {
-  const uint8_t roundConstants[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
+  static constexpr const uint8_t roundConstants[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
   switch(key.size()) {
     case 16: keysize = Aes128; break;
     case 24: // keysize = Aes192; break;
@@ -19,9 +19,10 @@ AesKeySchedule::AesKeySchedule(const std::vector<uint8_t>& key) {
   std::memcpy(eroundKeys, key.data(), key.size());
 
   for (std::size_t n = 0; n < 10; ++n) {
-      __m128i t = eroundKeys[n] ^ _mm_srli_si128(_mm_aeskeygenassist_si128 (eroundKeys[n] ,roundConstants[n]), 0x0c);
-      eroundKeys[n+1] = t ^ _mm_slli_si128 (t, 0x4) ^ _mm_slli_si128 (t, 0x8) ^ _mm_slli_si128 (t, 0xC);
+    auto t = eroundKeys[n] ^ _mm_srli_si128(_mm_aeskeygenassist_si128(eroundKeys[n], roundConstants[n]), 0x0c);
+    eroundKeys[n+1] = t ^ _mm_slli_si128 (t, 0x4) ^ _mm_slli_si128 (t, 0x8) ^ _mm_slli_si128 (t, 0xC);
   }
+
   for (size_t n = 0; n < 9; n++) {
       droundKeys[8 - n] = _mm_aesimc_si128(eroundKeys[n+1]);
   }
