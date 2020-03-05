eb288e9beec4c6d6f4eb15298dbc1c32312794e4
Peter Bindels <peter.bindels@tomtom.com>
2020-03-05 09:43:20 +0100

    Switch to Clang, fix constexpr issues. Update ghash.

--- occlude/cipher/include/occlude/cipher/ghash.h
+++ occlude/cipher/include/occlude/cipher/ghash.h
@@ -18,6 +18,7 @@ struct block {
   constexpr block operator<<(int count) { return b << count; }
   constexpr block operator>>(int count) { return b >> count; }
   constexpr block& operator^=(block a) { b ^= a.b; return *this; }
+  constexpr block operator|(block a) noexcept { return b | a.b; }
   friend constexpr block operator^(block a, block b) noexcept {
     return block(a.b ^ b.b);
   }
@@ -27,53 +28,37 @@ struct block {
   }
 };
 
-constexpr block reduction = { 0, 0b1000'0111 };
+constexpr block reduction = { static_cast<int64_t>(0xC200'0000'0000'0000), 0 };
 
-constexpr uint64_t tops = 0xaaaaaaaaaaaaaaaa;
-constexpr uint64_t bottoms = 0x5555555555555555;
-
-constexpr block clmul(block a, block b, uint8_t selector) {
-#ifdef __AES__
-//  if constexpr(std::is_constant_evaluated()) {
-  if constexpr(true) {
-#endif
-    __m128i a_ = { 0, (selector & 1) ? a.b[1] : a.b[0] };
-    __m128i b_ = { 0, (selector & 0x10) ? b.b[1] : b.b[0] };
-    __m128i t = ((a_ & tops) * (b_ & tops));
-    __m128i b = ((a_ & bottoms) * (b_ & bottoms));
-    t[0] &= tops;
-    t[1] &= tops;
-    b[0] &= bottoms;
-    b[1] &= bottoms;
-    return t | b;
-#ifdef __AES__
-  } else {
-    return _mm_clmulepi64_si128(a, b, selector);
-  }
-#endif
-}
-
-constexpr block galoisMultiply(block xy, block h) {
+block galoisMultiply(block xy, block h) {
   // Use 4 64-bit clmul's to do a 128-bit clmul
-  block resultLow = clmul(xy, h, 0x00);
-  block resultMid1 = clmul(xy, h, 0x10);
-  block resultMid2 = clmul(xy, h, 0x01);
-  block resultHigh = clmul(xy, h, 0x11);
+  block resultLow = _mm_clmulepi64_si128(xy, h, 0x00);
+  block resultMid = _mm_clmulepi64_si128(xy, h, 0x10) ^ _mm_clmulepi64_si128(xy, h, 0x01);
+  block resultHigh = _mm_clmulepi64_si128(xy, h, 0x11);
+
   // Combine half results
-  resultLow ^= (resultMid1 << 64) ^ (resultMid2 << 64);
-  resultHigh ^= (resultMid1 >> 64) ^ (resultMid2 >> 64);
-  // Take high result and reduce it with polynomial
-  block overflowLow = clmul(resultHigh, reduction, 0x00);
-  block overflowHigh = clmul(resultHigh, reduction, 0x01);
-  // This gives us a 135 bit result. Take the bottom 128 and add it in
-  resultLow ^= overflowLow ^ (overflowHigh << 64);
-  // then take the top 7 bit result, reduce that again (to a 14-bit result), and add it in too.
-  resultLow ^= clmul(overflowHigh, reduction, 0x01);
-  // since add cannot overflow, this is now the 128-bit result
-  return resultLow; 
+  resultLow.b[1] ^= resultMid.b[0];
+  resultHigh.b[0] ^= resultMid.b[1];
+
+  block tmp7 = _mm_srli_epi32(resultLow, 31);
+  block tmp8 = _mm_srli_epi32(resultHigh, 31);
+  resultLow = _mm_slli_epi32(resultLow, 1);
+  resultHigh = _mm_slli_epi32(resultHigh, 1);
+
+  resultLow = resultLow | _mm_slli_si128(tmp7, 4);
+  resultHigh = resultHigh | _mm_srli_si128(tmp7, 12) | _mm_slli_si128(tmp8, 4);
+
+  block ovLow = _mm_clmulepi64_si128(resultLow, reduction, 0x00);
+  resultLow.b[1] ^= ovLow.b[0];
+
+
+  resultHigh.b[0] ^= ovLow.b[1];
+  resultHigh ^= _mm_clmulepi64_si128(resultLow, reduction, 0x01) ^ resultLow;
+
+  return resultHigh;
 }
 
-constexpr block ghash_block(block x, block h, block hash) {
+block ghash_block(block x, block h, block hash) {
   return galoisMultiply(hash ^ x, h);
 }
 
--- occlude/cipher/src/aes.cpp
+++ occlude/cipher/src/aes.cpp
@@ -9,8 +9,14 @@
 
 namespace Occlude::Cipher {
 
+template <uint8_t rconstant>
+static __m128i nextRoundKey(__m128i in) {
+  auto t = in ^ _mm_srli_si128(_mm_aeskeygenassist_si128(in, rconstant), 0x0c);
+  return t ^ _mm_slli_si128 (t, 0x4) ^ _mm_slli_si128 (t, 0x8) ^ _mm_slli_si128 (t, 0xC);
+}
+
 AesKeySchedule::AesKeySchedule(const std::vector<uint8_t>& key) {
-  static constexpr const uint8_t roundConstants[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
+  constexpr const uint8_t roundConstants[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
   switch(key.size()) {
     case 16: keysize = Aes128; break;
     case 24: // keysize = Aes192; break;
@@ -19,10 +25,16 @@ AesKeySchedule::AesKeySchedule(const std::vector<uint8_t>& key) {
   }
   std::memcpy(eroundKeys, key.data(), key.size());
 
-  for (std::size_t n = 0; n < 10; ++n) {
-    auto t = eroundKeys[n] ^ _mm_srli_si128(_mm_aeskeygenassist_si128(eroundKeys[n], roundConstants[n]), 0x0c);
-    eroundKeys[n+1] = t ^ _mm_slli_si128 (t, 0x4) ^ _mm_slli_si128 (t, 0x8) ^ _mm_slli_si128 (t, 0xC);
-  }
+  eroundKeys[1] = nextRoundKey<roundConstants[0]>(eroundKeys[0]);
+  eroundKeys[2] = nextRoundKey<roundConstants[1]>(eroundKeys[1]);
+  eroundKeys[3] = nextRoundKey<roundConstants[2]>(eroundKeys[2]);
+  eroundKeys[4] = nextRoundKey<roundConstants[3]>(eroundKeys[3]);
+  eroundKeys[5] = nextRoundKey<roundConstants[4]>(eroundKeys[4]);
+  eroundKeys[6] = nextRoundKey<roundConstants[5]>(eroundKeys[5]);
+  eroundKeys[7] = nextRoundKey<roundConstants[6]>(eroundKeys[6]);
+  eroundKeys[8] = nextRoundKey<roundConstants[7]>(eroundKeys[7]);
+  eroundKeys[9] = nextRoundKey<roundConstants[8]>(eroundKeys[8]);
+  eroundKeys[10] = nextRoundKey<roundConstants[9]>(eroundKeys[9]);
 }
 
 AesDecryptKeySchedule::AesDecryptKeySchedule(const AesKeySchedule& key)
